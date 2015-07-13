2d296a83233808bd68188f45f0988a59b66ca764
mininet <lyndonfawcett@hotmail.com>
2015-07-13 15:31:00 -0700

    first commit

---
+++ remote-command-server.py
@@ -0,0 +1,22 @@
+import pyjsonrpc
+import subprocess
+
+class RequestHandler(pyjsonrpc.HttpRequestHandler):
+
+  @pyjsonrpc.rpcmethod
+  def do_command(self, command):
+
+
+    output = subprocess.check_output(command, shell=True)
+
+    return output
+
+
+# Threading HTTP-Server
+http_server = pyjsonrpc.ThreadingHttpServer(
+    server_address = ('localhost', 8080),
+    RequestHandlerClass = RequestHandler
+)
+print "Starting HTTP server ..."
+print "URL: http://localhost:8080"
+http_server.serve_forever()
